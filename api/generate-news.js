3d52d6105ee90c77b953f13c8aa543eacd14dd22
aaalllkkk <muhamadmalik9850@gmail.com>
2026-04-02 02:17:14 +0700

    Update generate-news.js

--- api/generate-news.js
+++ api/generate-news.js
@@ -18,7 +18,7 @@ Score: ${score}
 `;
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1beta/models/Gemini 3.1 Flash Lite:generateContent?key=" +
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite-preview:generateContent?key=" +
         process.env.GEMINI_API_KEY,
       {
         method: "POST",
