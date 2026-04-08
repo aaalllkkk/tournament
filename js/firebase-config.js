00bb39bd8f0400ef7211a7de9923ab37c9d0b94c
aaalllkkk <muhamadmalik9850@gmail.com>
2026-04-09 01:42:20 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAmlSol_3Wmfs53A98sEdEm-uSaByGzo28",
-  authDomain: "tournament-sys.firebaseapp.com",
-  projectId: "tournament-sys",
-  storageBucket: "tournament-sys.firebasestorage.app",
-  messagingSenderId: "519201807872",
-  appId: "1:519201807872:web:9054f639cbd8190d9ec2e5"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
