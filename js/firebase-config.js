4cb74cb9954e2b6115bb6be442b2a5df2330c677
aaalllkkk <muhamadmalik9850@gmail.com>
2026-04-09 01:56:27 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,7 @@
-// File: js/firebase-config.js
+// file: js/firebase-config.js
+async function getFirebaseConfig() {
+    const response = await fetch('/api/config');
+    return await response.json();
+}
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
-};
-
-const app = initializeApp(firebaseConfig);
-
-// Kita 'export' db dan auth supaya file lain bisa meminjamnya
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export { getFirebaseConfig };
